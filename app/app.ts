a4a23dd6595cb75a1c1b515b34fdb4417a206dfa
Eliu Florez <eliufz@gmail.com>
2016-07-28 15:53:42 -0430

    Fixed ion-tabs

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,16 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HomePage} from './pages/home/home';
-import {LoginPage} from './pages/login/login';
+import { Component } from '@angular/core';
+import { Platform, ionicBootstrap } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+import { TabsPage } from './pages/tabs/tabs';
+import { LoginPage } from './pages/login/login';
 import * as firebase from 'firebase';
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
 export class MyApp {
-  rootPage: any = HomePage;
+
+  rootPage: any;
 
   constructor(platform: Platform) {
 		
@@ -25,7 +26,7 @@ export class MyApp {
 		firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // If there's a user take him to the home page.
-        this.rootPage = HomePage;
+        this.rootPage = TabsPage;
       } else {
         // If there's no user logged in send him to the LoginPage
         this.rootPage = LoginPage;
